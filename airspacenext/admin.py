01e2a9fba60b9b26b5a707e8395a0c76aad477cc
ca <achinga.chris@gmail.com>
2024-02-17 16:27:10 +0300

    format files

--- airspacenext/admin.py
+++ airspacenext/admin.py
@@ -5,12 +5,12 @@ from .models import Airport, Airline
 
 @admin.register(Airport)
 class AirportAdmin(ImportExportModelAdmin):
-    list_display = ('name', 'icao', 'iata', 'country')
-    list_filter = ('country',)
-    search_fields = ('name', 'icao', 'iata', 'country')
+    list_display = ("name", "icao", "iata", "country")
+    list_filter = ("country",)
+    search_fields = ("name", "icao", "iata", "country")
 
 
 @admin.register(Airline)
 class AirlineAdmin(ImportExportModelAdmin):
-    list_display = ('name', 'code', 'icao')
-    search_fields = ('name', 'code', 'icao')
+    list_display = ("name", "code", "icao")
+    search_fields = ("name", "code", "icao")
--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-admin.site.site_header = 'Chris Achinga'
-admin.site.site_title = 'Chris Achinga'
-admin.site.index_title = 'Welcome to Chris Achinga'
+admin.site.site_header = "Chris Achinga"
+admin.site.site_title = "Chris Achinga"
+admin.site.index_title = "Welcome to Chris Achinga"
